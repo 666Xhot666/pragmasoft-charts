78f117208ac38149c94751ac64365ff1bb61c1e9
Jack <666xhot666@gmail.com>
2021-02-12 23:43:04 +0200

    modify App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useCallback, useState, useEffect } from 'react'
+
+import crossfilter from 'crossfilter2'
+
+import { NavBarComponent } from './components'
+import { MainPage } from './pages'
+
 import { useGetData } from './hooks'
+
+import { NdxState } from './types'
 
 export const App = (): JSX.Element => {
   const { getData } = useGetData()
+  const [ndx, setNdx] = useState<NdxState>()
 
-  getData().then(console.log)
+  const fetchData = useCallback(async () => {
+    const data = await getData()
+    const ndx = crossfilter(data)
+    setNdx(ndx)
+  }, [getData])
 
-  return <div className='App'>App</div>
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
+
+  return (
+    <>
+      <NavBarComponent />
+      <MainPage ndx={ndx as NdxState} />
+    </>
+  )
 }
