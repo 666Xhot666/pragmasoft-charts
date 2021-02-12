1a355833ce02f7b6d33f32b96e1cb9b1b78da92a
Jack <666xhot666@gmail.com>
2021-02-12 23:40:55 +0200

    add Navbar component

---
+++ src/components/NavBar.component/Navbar.component.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const NavBarComponent = () => {
+  return (
+    <nav className='blue darken-2'>
+      <div className='container'>
+        <div className='brand-logo center'>Pragmasoft Charts</div>
+      </div>
+    </nav>
+  )
+}
