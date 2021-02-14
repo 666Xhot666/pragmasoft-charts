c14577c96456bce2f39e8bd94c833b805e4f4094
Jack <666xhot666@gmail.com>
2021-02-15 01:06:43 +0200

    update  Switch Bar

--- src/components/SwitchBar.component.tsx
+++ src/components/SwitchBar.component.tsx
@@ -1,30 +1,56 @@
-import React from 'react'
-import M from 'materialize-css'
+import React, { useCallback, useEffect } from 'react'
+import { dataCount, filterAll, refocusAll, renderAll } from 'dc'
 
 import { PropsSwitchBar } from '../types'
 
-export const SwitchBarComponent: React.FC<PropsSwitchBar> = (params) => {
-  const { resetHandler, dropdownPropertyHandler } = params
+import M from 'materialize-css'
+
+export const SwitchBarComponent: React.FC<PropsSwitchBar> = (
+  params
+): JSX.Element => {
+  const { dropdownPropertyHandler } = params
 
   //markdown || revenues || margin
   document.addEventListener('DOMContentLoaded', () => {
     M.FormSelect.init(document.querySelectorAll('select'))
   })
 
+  const updateCount = useCallback((): void => {
+    const { ndx } = params
+    if (!ndx) return
+    const dataCountHandler = dataCount('.data-count')
+    dataCountHandler.dimension(ndx).group(ndx.groupAll())
+    return dataCountHandler.render()
+  }, [params])
+
+  useEffect(() => {
+    updateCount()
+  }, [updateCount])
+
   return (
     <div className='switch-bar center-align row'>
       <div className='select col s12 m6 l2'>
+        <label>Parameter Select</label>
         <select id='parameters' onChange={dropdownPropertyHandler}>
           <option value='markdown'>markdown</option>
           <option value='revenues'>revenues</option>
           <option value='margin'> margin</option>
         </select>
-        <label htmlFor='parameters'>Parameter Select</label>
+      </div>
+      <div className='data-count  col s12  m6 l3 offset-l2'>
+        <span className='filter-count' />
+        {' selected of '}
+        <span className='total-count' />
+        {' records!'}
       </div>
       <div className='button col s12 m6 l2 offset-l8'>
         <button
-          className='btn-large btn-floating orange darken-4'
-          onClick={resetHandler}
+          className='btn-large  green darken-2 waves-effect'
+          onClick={() => {
+            filterAll()
+            refocusAll()
+            renderAll()
+          }}
         >
           Reset all
         </button>
